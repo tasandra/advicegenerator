96efcb8e6505bb7c109ca18975f30c93d6215474
tasandra <tasandra40@gmail.com>
2023-06-04 11:52:32 -0700

    add loading page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container">
+      <h3>
+        Advice #{" "}
+        {data ? <span>{data.slip.id}</span> : <span>Loading data...</span>}
+      </h3>
+
+      <blockquote>{data ? <q>{data.slip.advice}</q> : <q>Loading data...</q>}</blockquote>
+      <div className="divider">
+        <span className="line"></span>
+        <img src="/images/pattern-divider-desktop.svg" alt="divider" />
+        <span className="line"></span>
+      </div>
+      <div className="icon">
+        <a href="reload">
+          <img src="/images/icon-dice.svg" alt="icon" />
         </a>
-      </header>
+      </div>
     </div>
   );
 }
