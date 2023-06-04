481dc358a832b006981028ce977b46cc35575327
tasandra <tasandra40@gmail.com>
2023-06-04 14:13:42 -0700

    add refresh page

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ function App() {
     }
   };
 
+  const refreshPage = ()=>{
+    window.location.reload();
+ }
+
   return (
     <div className="container">
       <h3>
@@ -33,9 +37,9 @@ function App() {
         </picture>
       </div>
       <div className="icon">
-        <a href="reload">
+        <button onClick={refreshPage}>
           <img src="/images/icon-dice.svg" alt="icon" />
-        </a>
+        </button>
       </div>
     </div>
   );
